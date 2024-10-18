9017d7ca68430c23e508abc1e0e6eaa8095428e5
asus <asus@SHAIRIMIRZA>
2024-10-19 02:54:49 +0800

    Add project files.

---
+++ YouTubeApiProjects/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YouTubeApiProjects.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
